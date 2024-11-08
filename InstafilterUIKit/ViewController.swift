b3f157d74acb27fa7983bcb46d3e7acac383dfa5
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-11-08 19:15:02 -0300

    feat: Add save image to photo gallery functionality

--- InstafilterUIKit/ViewController.swift
+++ InstafilterUIKit/ViewController.swift
@@ -50,6 +50,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+        
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
@@ -118,6 +123,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             // Assign ui image to the imageView
             imageView.image = processedImage
         }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            let ac = UIAlertController(title: "Save Error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "image has been saved", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
     }
 }
 
